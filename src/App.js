64d079f523b63c5d95c682d4ae234d762d57cd33
Ronaldo Arruda <nronaldo_7@hotmail.com>
2022-05-20 13:27:44 -0300

    main-page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import { useForm } from "react-hook-form"
+
 
 function App() {
+  const { register, handleSubmit,  formState: { errors } } = useForm();
+  const onSubmit = data => console.log(data, errors);
+  const isEmpty = Object.keys(errors).length === 0;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='background-page'>
+        <img
+        src="logo-art-you.png"
+        alt="Logo"
+        id='logo-one-blue'/>
+        <div className='content-text'>
+            <h1>Every day we help artists, galleries and museums thrive in a new digital world</h1>
+            <h2>Get started today with our specialized help</h2>
+        </div>
+      </div>
+      <form className='sign-in' onSubmit={handleSubmit(onSubmit)}>
+        <div className='input-login'>
+          <h1>Login</h1>
+          <input
+          type="text"
+          id='input-login'
+          placeholder='Username'
+          {...register("username", {required:true})}/>
+          <input
+          type="password"
+          id='input-login'
+          placeholder='Password'
+          {...register("password", {required:true})}/>
+          {(errors.password || errors.username)  && <span>Digit your username and password!</span>}
+        </div>
+        <div className='action'>
+          <button type='submit' disabled={!isEmpty} id="button-login">Log In</button>
+          <button type='button'>Sign up</button>
+        </div>
+      </form>
     </div>
   );
 }
