206f1bcc94db5a3f97d66ee4bf61840e0e841a7f
Ronaldo Arruda <nronaldo_7@hotmail.com>
2022-05-21 17:58:21 -0300

    validações

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -2,19 +2,31 @@ import './index.css';
 import { useForm } from "react-hook-form";
 import api from "../../services/api";
 import { useState } from 'react';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as Yup from 'yup'
+import { Link } from "react-router-dom";
 
 
 function Login() {
+
+  const formSchema = Yup.object().shape({
+    name: Yup.string()
+      .required('Username is mendatory'),
+    password: Yup.string()
+      .required('Password is mendatory'),
+  })
+
+  const formOptions = {resolver: yupResolver(formSchema)}
+
   const [isError, setIsError] = useState(false)
 
-  const { register, handleSubmit,  formState: { errors } } = useForm();
+  const { register, handleSubmit,  formState: { errors } } = useForm(formOptions);
   const onSubmit = async data => {
     setIsError(false)
     const response = await api.post("/login", data).catch((e) => {setIsError(!e.response.data.ok)})
-    console.log(response.data.ok);
+    console.log(response.data);
 
   };
-  const isEmpty = Object.keys(errors).length === 0;
 
   return (
     <form className='sign-in' onSubmit={handleSubmit(onSubmit)}>
@@ -25,19 +37,22 @@ function Login() {
         id='input-name'
         placeholder='Username'
         {...register("name", {required:true})}
-        onChange={() => setIsError(false)}/>
+        onChange={() => setIsError(false)}
+        className={`form-control ${errors.name ? 'is-invalid' : ''}`}/>
+        <div className="invalid-feedback">{errors.name?.message}</div>
         <input
         type="password"
         id='input-password'
         placeholder='Password'
         {...register("password", {required:true})}
-        onChange={() => setIsError(false)}/>
-        {(errors.password || errors.username)  && <span>Digit your username and password!</span>}
+        onChange={() => setIsError(false)}
+        className={`form-control ${errors.password ? 'is-invalid' : ''}`}/>
+         <div className="invalid-feedback">{errors.password?.message}</div>
         { isError && <span>The username you entered doesn't belong to an account. Please check your username and try again.</span>}
       </div>
       <div className='action'>
-        <button type='submit' disabled={!isEmpty} id="button-login" >Log In</button>
-        <button type='button'>Sign up</button>
+        <button type='submit' id="button-login" >Log In</button>
+        <Link to="/sign-up" ><button className='sign-up-btn'>Sign up</button></Link>
       </div>
     </form>
   );
--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,25 +1,63 @@
 import './index.css';
+import { useForm } from "react-hook-form";
+import { yupResolver} from '@hookform/resolvers/yup'
+import * as Yup from 'yup'
+import api from '../../services/api';
+import { useState } from 'react';
+
 
 function SignUp() { 
+  // verificar igualdade das senhas
+  const formSchema = Yup.object().shape({
+    name: Yup.string()
+      .required('Username is mendatory')
+      .min(6, 'Username must be at 4 char long'),
+    password: Yup.string()
+      .required('Password is mendatory')
+      .min(4, 'Password must be at 4 char long'),
+    confirmPassword: Yup.string()
+      .required('Password is mendatory')
+      .oneOf([Yup.ref('password')], 'Passwords does not match')
+  })
+  const formOptions = {resolver: yupResolver(formSchema)}
+
+  const { register, handleSubmit, formState: { errors } } = useForm(formOptions); 
+
+  const [isError, setIsError] = useState(false)
+  const onSubmit = async data => {
+    setIsError(false)
+    const response = await api.post("/user/cadaster", data).catch((e) => {setIsError(!e.response.data.ok)})
+    console.log(response.data.ok)
+  }
+
   return (
-    <form className='sign-up'>
+    <form className='sign-up' onSubmit={handleSubmit(onSubmit)}>
       <div className='input-sign-up'>
-        <h1>Sign-Up</h1>
+        <h1>Sign Up</h1>
         <input
         type='text'
         id='input-name'
-        placeholder='Username'/>
+        placeholder='Username'
+        {...register("name", {required:true})}
+        className={`form-control ${errors.name ? 'is-invalid' : ''}`}/>
+        <div className='invalid-feedback'>{errors.name?.message}</div>
         <input
         type='password'
         id='input-password'
-        placeholder='Password'/>
+        placeholder='Password'
+        {...register("password", {required:true})}
+        className={`form-control ${errors.password ? 'is-invalid' : ''}`}/>
+        <div className='invalid-feedback'>{errors.password?.message}</div>
         <input
         type='password'
         id='input-new-password'
-        placeholder='Repeat your password'/>
+        placeholder='Repeat your password'
+        {...register("confirmPassword", {required:true})}
+        className={`form-control ${errors.confirmPassword ? 'is-invalid' : ''}`}/>
+        <div className='invalid-feedback'>{errors.confirmPassword?.message}</div>
       </div>
       <div className='action'>
-        <button>Sign-Up</button>
+        <button type='submit'>Sign Up</button>
       </div>
     </form>
   );
